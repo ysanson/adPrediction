d952f18962946a06d47e22a6963a5522732b3456
Unknown <mariesalelles@gmail.com>
2019-10-29 17:03:39 +0100

    fix bug pull rebase

--- src/main/scala/SassWI/LogisticRegression.scala
+++ src/main/scala/SassWI/LogisticRegression.scala
@@ -15,25 +15,25 @@ object LogisticRegression {
   def logisticRegressionMethod(df: sql.DataFrame): Unit = {
     //load data in RDD labelPoint
     //val data = MLUtils.loadLibSVMFile(new SparkContext( new SparkConf()), "path")
-    val data: RDD[Row] = df.select("features", "labelIndex").limit(250).rdd
+    val data: RDD[Row] = df.select("features", "labelIndex").limit(100).rdd
     //transform row into labeledPoint
     val d: RDD[LabeledPoint] = data.map((l:Row) => LabeledPoint( l.getAs(1), Vectors.fromML(l.getAs(0)).toDense))
 
     // Split data into 2 datasets: training data and test data
-    //val splits = d.randomSplit(Array(0.7, 0.3), seed = 11L)
-    //val trainingData = splits(0).cache()
-    //val testData = splits(1)
+    val splits = d.randomSplit(Array(0.7, 0.3), seed = 11L)
+    val trainingData = splits(0).cache()
+    val testData = splits(1)
 
     // model creation
-    val model = new LogisticRegression()
-      .fit(data)
+    val model = new LogisticRegressionWithLBFGS()
+      .setNumClasses(2)
+      .run(trainingData)
 
     // computation onto the test data
-    /*val predictionAndLabels = testData.map { case LabeledPoint(label, features) =>
+    val predictionAndLabels = testData.map { case LabeledPoint(label, features) =>
       val prediction = model.predict(features)
       (prediction, label)
-    }*/
-    val predictions = model.transform(data)
+    }
 
     //add accuracy of the model
     val metrics = new MulticlassMetrics(predictionAndLabels)
