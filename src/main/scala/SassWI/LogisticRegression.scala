1af976966db6d7fa88759144ad213263bc2678d9
Unknown <mariesalelles@gmail.com>
2019-10-29 16:56:04 +0100

    logistic regression works !!!

--- src/main/scala/SassWI/LogisticRegression.scala
+++ src/main/scala/SassWI/LogisticRegression.scala
@@ -15,11 +15,10 @@ object LogisticRegression {
   def logisticRegressionMethod(df: sql.DataFrame): Unit = {
     //load data in RDD labelPoint
     //val data = MLUtils.loadLibSVMFile(new SparkContext( new SparkConf()), "path")
-    //val data: RDD[Row] = df.select("features", "labelIndex").limit(20).rdd
+    val data: RDD[Row] = df.select("features", "labelIndex").limit(250).rdd
     //transform row into labeledPoint
-    //val d: RDD[LabeledPoint] = data.map((l:Row) => LabeledPoint(l.fieldIndex("labelIndex"), Vectors.dense(l.fieldIndex("features"))))
+    val d: RDD[LabeledPoint] = data.map((l:Row) => LabeledPoint( l.getAs(1), Vectors.fromML(l.getAs(0)).toDense))
 
-    val data = df.select("labelIndex", "features").withColumnRenamed("labelIndex", "label")
     // Split data into 2 datasets: training data and test data
     //val splits = d.randomSplit(Array(0.7, 0.3), seed = 11L)
     //val trainingData = splits(0).cache()
@@ -37,12 +36,19 @@ object LogisticRegression {
     val predictions = model.transform(data)
 
     //add accuracy of the model
-    val evaluator = new MulticlassClassificationEvaluator()
-      .setLabelCol("label")
-      .setPredictionCol("prediction")
-      //.setMetricName("precision")
-    val accuracy = evaluator.evaluate(predictions)
-    println("Test Error = " + (1.0 - accuracy))
+    val metrics = new MulticlassMetrics(predictionAndLabels)
+    val accuracy = metrics.accuracy
+    println(s"Accuracy = $accuracy")
+
+    //confusion matrix
+    println("Confusion matrix:")
+    println(metrics.confusionMatrix)
+
+    //precision y labels (click or not click)
+    val labels = metrics.labels
+    labels.foreach { l =>
+      println(s"Precision($l) = " + metrics.precision(l))
+    }
 
     // Save and load model
 
